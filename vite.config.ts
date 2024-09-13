4a6aa7b30edbd84f0146a56cb4c6ff3a9105c482
Jeff Eganhouse <jeganhouse@gala.games>
2024-09-12 22:11:46 -0400

    deployment settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'url'
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      port: 3000
+    },
+    base: '/gala-faucet/',
+    define: {
+      __APP_ENV__: env.APP_ENV
     }
-  },
-  server: {
-    port: 3000
   }
 })
